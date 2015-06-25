6689a8fa58c8a94a90198ebe68b159e08904086f
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2015-06-25 18:44:45 +0200

    Add Android.mk files to build lz4 as host, target modules, also added target static and shared liblz4 module

---
+++ lib/Android.mk
@@ -0,0 +1,26 @@
+# Copyright (C) 2015 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+
+liblz4_src_files := lz4.c lz4hc.c lz4frame.c xxhash.c
+liblz4_common_cflags := -O3 -std=c99 -Wall -Wextra -Wundef -Wshadow -Wcast-align -Wstrict-prototypes -pedantic
+
+LOCAL_ARM_MODE := arm
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_SRC_FILES := $(liblz4_src_files)
+LOCAL_CFLAGS := $(liblz4_common_cflags)
+include $(BUILD_HOST_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_SRC_FILES := $(liblz4_src_files)
+LOCAL_CFLAGS := $(liblz4_common_cflags)
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := liblz4
+LOCAL_SRC_FILES := $(liblz4_src_files)
+LOCAL_CFLAGS := $(liblz4_common_cflags)
+include $(BUILD_STATIC_LIBRARY)
---
+++ programs/Android.mk
@@ -0,0 +1,27 @@
+# Copyright (C) 2015 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+
+lz4_common_c_includes := $(LOCAL_PATH)/../lib
+lz4_common_cflags := -O3 -std=c99 -Wall -Wextra -Wundef -Wshadow -Wcast-align -Wstrict-prototypes -pedantic -DLZ4_VERSION=\"r131\"
+lz4_common_src_files := bench.c lz4cli.c lz4io.c
+
+LOCAL_ARM_MODE := arm
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lz4
+LOCAL_C_INCLUDES := $(lz4_common_c_includes)
+LOCAL_CFLAGS := $(lz4_common_cflags)
+LOCAL_SRC_FILES := $(lz4_common_src_files)
+LOCAL_STATIC_LIBRARIES := liblz4
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lz4
+LOCAL_C_INCLUDES := $(lz4_common_c_includes)
+LOCAL_CFLAGS := $(lz4_common_cflags)
+LOCAL_SRC_FILES := $(lz4_common_src_files)
+LOCAL_STATIC_LIBRARIES := liblz4
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_HOST_EXECUTABLE)
